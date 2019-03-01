77d32e47ba3553eba72ea5e9b88de938ddc5b9ff
lucas-vieira <lucas.vieira@aluno.uece.br>
2019-03-01 15:00:23 -0300

    final commit acceleo project

---
+++ project/org.eclipse.acceleo.module.diagramadeatividades/src-gen/Atividade.java
@@ -0,0 +1,51 @@
+package org.eclipse.acceleo.module.diagramadeatividades.main.src-gen;
+	
+public class Atividade {
+
+	public void run(){
+
+					atividade1()asda;
+					
+				
+
+
+
+				
+
+					atividade2()asda;
+					
+				
+
+
+					atvidade3()asda;
+					
+				
+
+
+
+				
+
+
+				
+
+
+
+
+	}
+
+	public void atividade1(){
+	// TODO Auto-generated method stub
+
+	}
+	public void atividade2(){
+	// TODO Auto-generated method stub
+
+	}
+	public void atvidade3(){
+	// TODO Auto-generated method stub
+
+	}
+
+
+
+}
